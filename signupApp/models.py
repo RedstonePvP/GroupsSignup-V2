3fb8830fabfa6629d989d3905838c7a4ef44bb0e
RedstonePvP <avineedswifi@gmail.com>
2021-06-28 11:05:28 -0400

    created models for the main signup app

--- signupApp/models.py
+++ signupApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class group(models.Model):
+    name = models.CharField(max_length=200)
+    max = models.IntegerField()
+    active = models.BooleanField()
+
+class event(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    groups = models.ManyToManyField(group)
+    active = models.BooleanField()
+
+class externalEvent(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    url = models.URLField()
+    active = models.BooleanField()
