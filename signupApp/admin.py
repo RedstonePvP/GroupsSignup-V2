f2b094e86aabac34a687aa505a71357019099aaa
RedstonePvP <avineedswifi@gmail.com>
2021-06-28 11:15:45 -0400

    setup admin

--- signupApp/admin.py
+++ signupApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import group, event, externalEvent
 
 # Register your models here.
+admin.site.register(group)
+admin.site.register(event)
+admin.site.register(externalEvent)
--- signupApp/models.py
+++ signupApp/models.py
@@ -6,14 +6,23 @@ class group(models.Model):
     max = models.IntegerField()
     active = models.BooleanField()
 
+    def __str__(self):
+        return self.name
+
 class event(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     groups = models.ManyToManyField(group)
     active = models.BooleanField()
 
+    def __str__(self):
+        return self.title
+
 class externalEvent(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     url = models.URLField()
     active = models.BooleanField()
+
+    def __str__(self):
+        return self.title
