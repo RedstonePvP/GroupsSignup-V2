b71ffc37ee8467ff5d4dc274fc370c199541d739
RedstonePvP <avineedswifi@gmail.com>
2021-06-30 11:28:39 -0400

    finished template for register page. now to add functionality

--- signupApp/forms.py
+++ signupApp/forms.py
@@ -3,8 +3,8 @@ from django import forms
 class registerForm(forms.Form):
     firstName = forms.CharField(label='First Name', max_length=200, required=True)
     lastName = forms.CharField(label='Last Name', max_length=200, required=True)
-    age = forms.IntegerField(label="Age", required=True)
+    age = forms.IntegerField(label="Age", required=True, min_value=0, max_value=20)
     pickupName = forms.CharField(label='Pickup Person', max_length=200, required=True)
     parentLocation = forms.CharField(label='Parent Minyan', max_length=200, required=True)
     allergies = forms.CharField(label='Allergies', max_length=200, required=False)
-    address = forms.CharField(label='Address', max_length=200, required=True)
+    address = forms.CharField(label='Home Address', max_length=200, required=True)
