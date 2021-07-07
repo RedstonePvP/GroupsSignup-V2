08c2f79032495d18a3669a33f7abb942756d335f
RedstonePvP <avineedswifi@gmail.com>
2021-07-07 11:29:28 -0400

    fixed display bug on index.html

--- signupApp/views.py
+++ signupApp/views.py
@@ -6,10 +6,10 @@ from .models import group, event, externalEvent, regisration
 
 def indexView(response):
     q1 = event.objects.filter(active=True)
-    q1.filter(date__gte=datetime.date.today())
+    q1 = q1.filter(date__gte=datetime.date.today())
     
     q2 = externalEvent.objects.filter(active=True)
-    q2.filter(date__gte=datetime.date.today())
+    q2 = q2.filter(date__gte=datetime.date.today())
 
     allEvents = list(q1)+list(q2)
     allEvents.sort(key=lambda x: x.date, reverse=False)
